3b1399d91edf4d3dd32dc8ff9c1c2294b2b92a16
lídia <58692699+legolidia@users.noreply.github.com>
2023-04-04 16:54:28 +0000

    projeto finalizado (sem retoques)

--- wordleProject/wordle.c
+++ wordleProject/wordle.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <time.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 int main()
 {
@@ -10,11 +11,13 @@ int main()
     int verificaPalavra(char palavra[]);
     void wordle();
 
-    int menu = -1;
+    int menu = 0;
 
     while (menu > -1)
     {
-        printf("-------------WORDLE----------------\n");
+        printf("-------------GENIO----------------\n");
+        printf("1 - Iniciar Jogo\n");
+        printf("2 - Sair\n");
 
         printf("Digite a opção desejada: ");
         scanf("%d", &menu);
@@ -25,11 +28,12 @@ int main()
             wordle();
             break;
         case 2:
-            printf("Obrigada por jogar!\n");
-            printf("Desenvolvido por:\n");
-            printf("Isabella de Castro Jorge - TIA\n");
-            printf("Lídia Carolina de Andrade Rosa\n");
-            printf("Turma 02G11 - FCI - Ciência da Computação\n");
+            printf("\n          OBRIGADA POR JOGAR!\n\n");
+            printf("            ♡⸜(˃ ᵕ ˂ )⸝♡\n\n");
+            printf("           Desenvolvido por:\n\n");
+            printf("     Isabella de Castro Jorge - TIA\n");
+            printf("  Lídia Carolina de Andrade Rosa - 32281374\n\n");
+            printf("  Turma 02G11 - FCI - Ciência da Computação\n\n\n");
             menu = -1;
             break;
         default:
@@ -80,12 +84,12 @@ void sorteiaPalavra()
         {
             sorteada = fopen("sorteada.txt", "w");
             fprintf(sorteada, "%s", palavra);
-            printf("%s", palavra);
             tamanhoCerto = 0;
         }
     }
     rewind(palavras);
     fclose(palavras);
+    fclose(sorteada);
 }
 
 int verificaPalavra(char palavra[])
@@ -108,33 +112,93 @@ int verificaPalavra(char palavra[])
 
         if (strcmp(linha, palavra) == 0)
         {
-            printf("Palavra existe.\n");
             r = 1;
             break;
         }
 
-        else
+        else{
             r = 0;
+            printf("Palavra não existe, tente novamente. \n");
+        }
     }
 
     return r;
+
+    fclose(palavraSorteada);
+    fclose(todasPalavras);
 }
 
 void wordle()
 {
+    FILE *palavraSorteadaArquivo;
     char tentativa[6];
+    char resultado[6];
+
+    strcpy(resultado, "xxxxx");
+
     int numTentativas = 0;
 
     printf("Vamos começar!\n");
+    printf("Você tem 5 tentativas para tentar acertar a palavra. Apenas palavras que existem serão aceitas.\n");
+
     sorteiaPalavra();
+
+    char palavraSorteada[6];
+    palavraSorteadaArquivo = fopen("sorteada.txt", "r");
+
+    fgets(palavraSorteada, 6, palavraSorteadaArquivo);
+
+    fclose(palavraSorteadaArquivo);
 
     while (numTentativas < 5)
     {
-       do{
-        printf("Digite uma palavra de 5 letras: ");
-        scanf("%s", tentativa);
-       } while(strlen(tentativa) != 5);
+        do
+        {
+            printf("Digite uma palavra de 5 letras: ");
+            scanf("%s", tentativa);
+            if(strlen(tentativa) != 5)
+                printf("Essa palavra tem mais de 5 letras, tente novamente.\n");
+        } while(strlen(tentativa) != 5);
+
+        if (strcmp(tentativa, palavraSorteada) == 0)
+        {
+            printf("Parabéns, você acertou!\n");
+            break;
+        }
+
+        else
+        {
+            int i = 0;
+            for (i = 0; i < 5; i++)
+            {
+                if (tentativa[i] == palavraSorteada[i])
+                    resultado[i] = '^';
+                else
+                {
+                    int letrasIguais = 0;
+                    for (int j = 0; j < 5; j++){
+                        if (tentativa[i] == palavraSorteada[j])
+                            letrasIguais++;
+                    }
+                    if (letrasIguais > 0)
+                        resultado[i] = '!';
+                    else
+                        resultado[i] = 'x';
+                }
+            }
+
+            for(i=0; i<5;i++){
+                tentativa[i] = toupper(tentativa[i]);
+            }
+
+            printf("%s\n", tentativa);
+            printf("%s\n", resultado);
+
+            numTentativas++;
+            printf("Você tem mais %d tentativa(s).\n", 5 - numTentativas);
+        }
+    
     }
 
-    printf("ok.\n");
+    printf("A palavra era: %s\n", palavraSorteada);
 }
