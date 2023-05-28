ad2f358778c92c692dcdb40b0d5da90bae9c08f9
lídia <58692699+legolidia@users.noreply.github.com>
2023-05-28 04:53:25 +0000

    Finalização do projeto 2 e organização de pastas.

--- projeto2/dicionario.c
+++ projeto2/dicionario.c
@@ -1,34 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include<stdbool.h>
+#include <ctype.h>
 
-typedef char * string;
-
-#define NONE -1
-#define MAX_LENGTH 100
-#define NUM_STRINGS 2
-
-bool buscaBinaria(char* palavra, char list[][2], int tamanho)
-{
-  int inicio = 0;
-  int fim = tamanho - 1;
-  int meio;
-
-  while (inicio <= fim)
-  {
-    meio = (inicio + fim) / 2;
-
-    if (strcmp(palavra, list[meio]) < 0)
-      fim = meio - 1;
-    else if (strcmp(palavra, list[meio]) > 0)
-      inicio = meio + 1;
-    else
-      return meio;
-  }
-
-  return false;
-}
+void deixaMinuscula(char *palavra);
+void removeCaracteresEspeciais(char *palavra);
+int buscaBinaria(char[][45], int, char[], int *);
+void exibeDicionario();
 
 int main()
 {
@@ -36,29 +14,111 @@ int main()
   FILE *dicionario;
 
   txt = fopen("texto.txt", "r");
+
+  char palavra[45];
+  char vetorStrings[1000][45];
+  int tamanhoVetor = 0;
+
+  while (fscanf(txt, "%s", palavra) != EOF)
+  {
+
+    if (strlen(palavra) > 0)
+  {
+
+    deixaMinuscula(palavra);
+    removeCaracteresEspeciais(palavra);
+
+    int pos;
+    int achou = buscaBinaria(vetorStrings, tamanhoVetor, palavra, &pos);
+
+    if (achou == -1)
+    {
+      int i = 0;
+      for (i = tamanhoVetor; i > pos; i--)
+        strcpy(vetorStrings[i], vetorStrings[i - 1]);
+
+      strcpy(vetorStrings[pos], palavra);
+      tamanhoVetor++;
+    }
+  }
+  }
+
   dicionario = fopen("dicionario.txt", "w");
 
-  char palavra[50];
+  int i = 0;
 
-  int c = 0;
-
-  txt = fopen("texto.txt", "r");
-
-  while(!feof(txt)){
-  while (fscanf(txt, "%s", palavra) != EOF){
-    fprintf(dicionario, "%s\n", palavra);
-    c++;
-  }
-  }
-
-  while(!feof(dicionario)){
-    fgets(palavra, 50, dicionario);
-    printf("%s\n", palavra);
-  }
+  for (i = 0; i < tamanhoVetor; i++)
+    fprintf(dicionario, "%s\n", vetorStrings[i]);
 
   fclose(txt);
   fclose(dicionario);
 
+  exibeDicionario();
+
+  printf("total de palavras diferentes no dicionario=%d\n", tamanhoVetor);
 
   return 0;
+}
+
+void deixaMinuscula(char *palavra)
+{
+  int i = 0;
+  for (i = 0; palavra[i]; i++)
+    palavra[i] = tolower(palavra[i]);
+}
+
+void removeCaracteresEspeciais(char *palavra)
+{
+  int i = 0, j = 0;
+
+  while (palavra[i])
+  {
+    if (isalpha(palavra[i]))
+      palavra[j++] = palavra[i];
+
+    i++;
+  }
+
+  palavra[j] = '\0';
+}
+
+int buscaBinaria(char vetor[][45], int tam, char palavra[], int *pos)
+{
+  int inicio = 0;
+  int fim = tam - 1;
+  int meio;
+
+  while (inicio <= fim)
+  {
+    meio = (inicio + fim) / 2;
+    if (strcmp(palavra, vetor[meio]) == 0)
+      return meio;
+    else if (strcmp(palavra, vetor[meio]) < 0)
+      fim = meio - 1;
+    else
+      inicio = meio + 1;
+  }
+
+  *pos = inicio;
+
+  return -1;
+}
+
+void exibeDicionario()
+{
+
+  FILE *dicionario = fopen("dicionario.txt", "r");
+
+  char linha[45];
+  int i = 0;
+
+  while (fgets(linha, sizeof(linha), dicionario) != NULL)
+  {
+    if (linha[0] != '\n')
+    {
+      printf("%s", linha);
+    }
+  }
+
+  fclose(dicionario);
 }
--- wordleProject/wordle.c
+++ wordleProject/wordle.c
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 //Isabella de Castro Jorge - 42229960
-//LÌdia Carolina de Andrade Rosa - 32281374
+//Lidia Carolina de Andrade Rosa - 32281374
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,7 +21,7 @@ int main()
 		printf("1 - Iniciar Jogo\n");
 		printf("2 - Sair\n\n");
 
-		printf("Digite a opÁ„o desejada: ");
+		printf("Digite a opÔøΩÔøΩo desejada: ");
 		scanf("%d", &menu);
 
 
@@ -37,11 +36,11 @@ int main()
 			printf("           Desenvolvido por:\n\n");
 			printf("     Isabella de Castro Jorge - 42229960\n");
 			printf("  Lidia Carolina de Andrade Rosa - 32281374\n\n");
-			printf("  Turma 02G11 - FCI - CiÍncia da ComputaÁ„o\n\n\n");
+			printf("  Turma 02G11 - FCI - CiÔøΩncia da ComputaÔøΩÔøΩo\n\n\n");
 			menu = -1;
 			break;
 		default:
-			printf("\nOpÁ„o inv·lida, digite novamente.\n");
+			printf("\nOpÔøΩÔøΩo invÔøΩlida, digite novamente.\n");
 			printf("1 - Iniciar Jogo\n");
 			printf("2 - Sair\n");
 			scanf("%d", &menu);
@@ -151,8 +150,8 @@ void wordle()
 
 	int numTentativas = 0;
 
-	printf("\nVamos comeÁar!\n");
-	printf("VocÍ tem 6 tentativas para tentar acertar a palavra. Apenas palavras que existem ser„o aceitas.\n\n");
+	printf("\nVamos comeÔøΩar!\n");
+	printf("VocÔøΩ tem 6 tentativas para tentar acertar a palavra. Apenas palavras que existem serÔøΩo aceitas.\n\n");
 
 	sorteiaPalavra();
 
@@ -174,7 +173,7 @@ void wordle()
 
 		while(strlen(tentativa) != 5 || existe == 0)
 		{
-			printf("Palavra inv·lida, digite novamente.\n");
+			printf("Palavra invÔøΩlida, digite novamente.\n");
 			scanf("%s", &tentativa);
 			existe = verificaPalavra(tentativa);
 		}
@@ -231,7 +230,7 @@ void wordle()
 
 			numTentativas++;
 			if(numTentativas < 6)
-				printf("VocÍ tem mais %d tentativa(s).\n\n", 6 - numTentativas);
+				printf("VocÔøΩ tem mais %d tentativa(s).\n\n", 6 - numTentativas);
 		}
 
 	}
@@ -240,215 +239,3 @@ void wordle()
 		printf("A palavra era: %s\n\n", palavraSorteada);
 
 }
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <stdbool.h>
-#include <ctype.h>
-
-int main()
-{
-    void sorteiaPalavra();
-    int verificaPalavra(char palavra[]);
-    void wordle();
-
-    int menu = 0;
-
-    while (menu > -1)
-    {
-        printf("-------------GENIO----------------\n");
-        printf("1 - Iniciar Jogo\n");
-        printf("2 - Sair\n");
-
-        printf("Digite a op√ß√£o desejada: ");
-        scanf("%d", &menu);
-
-        switch (menu)
-        {
-        case 1:
-            wordle();
-            menu = 0;
-            break;
-        case 2:
-            printf("\n          OBRIGADA POR JOGAR!\n\n");
-            printf("            ‚ô°‚∏ú(ÀÉ ·µï ÀÇ )‚∏ù‚ô°\n\n");
-            printf("           Desenvolvido por:\n\n");
-            printf("     Isabella de Castro Jorge - TIA\n");
-            printf("  L√≠dia Carolina de Andrade Rosa - 32281374\n\n");
-            printf("  Turma 02G11 - FCI - Ci√™ncia da Computa√ß√£o\n\n\n");
-            menu = -1;
-            break;
-        default:
-            printf("Op√ß√£o inv√°lida, digite novamente.\n");
-            printf("1 - Iniciar Jogo\n");
-            printf("2 - Sair\n");
-            scanf("%d", &menu);
-            break;
-        }
-    }
-}
-
-void sorteiaPalavra()
-{
-    FILE *palavras;
-    FILE *sorteada;
-
-    palavras = fopen("palavras.txt", "r");
-
-    char palavra[20];
-    int i = 0;
-    while (!feof(palavras))
-    {
-        if (fgets(palavra, 20, palavras) != NULL)
-            i++;
-    }
-
-    srand(time(NULL));
-
-    rewind(palavras);
-
-    bool tamanhoCerto = 1;
-
-    while (tamanhoCerto)
-    {
-        rewind(palavras);
-        int sorteio;
-        sorteio = rand() % 6621;
-        i = 0;
-
-        while (i != sorteio)
-        {
-            fgets(palavra, 20, palavras);
-            i++;
-        }
-
-        if (strlen(palavra) == 6)
-        {
-            sorteada = fopen("sorteada.txt", "w");
-            fprintf(sorteada, "%s", palavra);
-            tamanhoCerto = 0;
-        }
-    }
-    rewind(palavras);
-    fclose(palavras);
-    fclose(sorteada);
-}
-
-int verificaPalavra(char palavra[])
-{
-    int i;
-    int r;
-    char linha[20];
-
-    FILE *todasPalavras;
-
-    todasPalavras = fopen("palavras.txt", "r");
-    rewind(todasPalavras);
-
-    while (!feof(todasPalavras))
-    {
-
-        fgets(linha, 20, todasPalavras);
-
-        printf("%s\n", linha);
-        
-        if (strcmp(linha, palavra) == 0 && strlen(linha) == 5)
-        {
-            r = 1;
-            break;
-        }
-
-        else{
-            r = 0;
-    
-        }
-    }
-
-    return r;
-
-    fclose(todasPalavras);
-}
-
-void wordle()
-{
-    FILE *palavraSorteadaArquivo;
-    char tentativa[6];
-    char resultado[6];
-
-    strcpy(resultado, "xxxxx");
-
-    int numTentativas = 0;
-
-    printf("Vamos come√ßar!\n");
-    printf("Voc√™ tem 5 tentativas para tentar acertar a palavra. Apenas palavras que existem ser√£o aceitas.\n");
-
-    sorteiaPalavra();
-
-    char palavraSorteada[6];
-    palavraSorteadaArquivo = fopen("sorteada.txt", "r");
-
-    fgets(palavraSorteada, 6, palavraSorteadaArquivo);
-
-    fclose(palavraSorteadaArquivo);
-
-    while (numTentativas < 5)
-    {
-        printf("Digite uma palavra de 5 letras: ");
-        scanf("%c", &teqwntativa);
-
-        int existe = verificaPalavra(tentativa);
-
-        while(strlen(tentativa) != 5 || existe == 0)
-        {
-            printf("Digite uma palavra de 5 letras: ");
-            scanf("%c", &tentativa);
-            existe = verificaPalavra(tentativa);
-        }
-        
-
-        if (strcmp(tentativa, palavraSorteada) == 0)
-        {
-            printf("Parab√©ns, voc√™ acertou!\n");
-            break;
-        }
-
-        else
-        {
-            int i = 0;
-            for (i = 0; i < 5; i++)
-            {
-                if (tentativa[i] == palavraSorteada[i])
-                    resultado[i] = '^';
-                else
-                {
-                    int letrasIguais = 0;
-                    for (int j = 0; j < 5; j++){
-                        if (tentativa[i] == palavraSorteada[j])
-                            letrasIguais++;
-                    }
-                    if (letrasIguais > 0)
-                        resultado[i] = '!';
-                    else
-                        resultado[i] = 'x';
-                }
-            }
-
-            for(i=0; i<5;i++){
-                tentativa[i] = toupper(tentativa[i]);
-            }
-
-            printf("%s\n", tentativa);
-            printf("%s\n", resultado);
-
-            numTentativas++;
-            if(numTentativas < 5)
-                printf("Voc√™ tem mais %d tentativa(s).\n", 5 - numTentativas);
-        }
-    
-    }
-
-    printf("A palavra era: %s\n", palavraSorteada);
-}
->>>>>>> 75482d545630bb90abbe4962357498e3f385becb
